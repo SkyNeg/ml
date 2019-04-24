57ac2aae57e7a5227f9d0d3674965537b429004a
Kirill Skripka <skyzap@gmail.com>
2019-04-24 19:26:36 +0300

    Add scripts for processing video files with Mask_RCNN

---
+++ mask_rcnn.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import random
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../")
